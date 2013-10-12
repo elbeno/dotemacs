40bf1f271c48ecd3a8dc7130e89403c8a70d5196
Ben Deane <ben@elbeno.com>
2013-10-11 22:16:53 -0700

    Convert fourcc or endianness at point.
    Add coloring for html color codes.

--- .emacs.d/autoloads/dec-to-hex.el
+++ .emacs.d/autoloads/dec-to-hex.el
@@ -1,8 +1,10 @@
+;; dec to hex conversion
+
 (defun dec-to-hex-at-point ()
   (interactive)
   (save-excursion
-    (skip-chars-backward "0123456789")
-    (or (looking-at "[0123456789]+")
+    (skip-chars-backward "0-9")
+    (or (looking-at "[0-9]+")
         (error "No number at point"))
     (replace-match (dec-to-hex (match-string 0)))))
 
@@ -28,3 +30,97 @@
     (error (dec-to-hex-at-point))))
 
 (global-set-key "\C-ch" 'toggle-dec-to-hex-at-point)
+
+;; fourcc conversion
+
+(defun fourcc-to-hex-at-point ()
+  (interactive)
+  (save-excursion
+    (skip-chars-backward "0-9a-zA-Z")
+    (or (looking-at "[0-9a-zA-Z]\\{1,4\\}\\b")
+        (error "No fourcc at point"))
+    (replace-match (fourcc-to-hex (match-string 0)) t)))
+
+(defun fourcc-to-hex (fourccstring)
+  (concat "0x"
+          (mapconcat (lambda (x) (format "%x" x))
+                     (mapcar 'string-to-char
+                             (mapcar 'string fourccstring))
+                     "")))
+
+(defun reverse-string (s)
+  (apply 'string (reverse (string-to-list s))))
+
+(defun collect-bytes-to-fourcc (l)
+  (cond ((null l) "")
+         ((= (length l) 1) (char-to-string (car l)))
+         (t (concat (char-to-string (+ (car l) (* 16 (cadr l))))
+                    (collect-bytes-to-fourcc (cddr l))))))
+
+(defun hex-to-fourcc (hexstring)
+  (reverse-string
+   (collect-bytes-to-fourcc
+    (mapcar (lambda (x) (string-to-number x 16))
+            (reverse (cddr (mapcar 'char-to-string hexstring)))))))
+
+(defun hex-to-fourcc-at-point ()
+  (interactive)
+  (save-excursion
+    (skip-chars-backward "x0123456789abcdefABCDEF")
+    (or (looking-at "0x[0-9a-fA-F]\\{1,8\\}\\b")
+        (error "No fourcc hex at point"))
+    (replace-match (hex-to-fourcc (match-string 0)) t)))
+
+(defun dec-to-fourcc-at-point ()
+  (interactive)
+  (save-excursion
+    (skip-chars-backward "0-9")
+    (or (looking-at "[0-9]+\\b")
+        (error "No number at point"))
+    (replace-match (hex-to-fourcc (dec-to-hex (match-string 0))) t)))
+
+(defun toggle-fourcc-to-hex-at-point ()
+  (interactive)
+  (condition-case nil
+      (dec-to-fourcc-at-point)
+    (error (condition-case nil
+                (hex-to-fourcc-at-point)
+              (error (fourcc-to-hex-at-point))))))
+
+(global-set-key "\C-cf" 'toggle-fourcc-to-hex-at-point)
+
+;; endian swap at point
+
+(defun string-to-pairs (s)
+  (cond ((= (length s) 0) nil)
+        ((= (length s) 1) (list s))
+        (t (cons (substring s 0 2) (string-to-pairs (substring s 2))))))
+
+(defun endian-swap (hexstring)
+  (cond ((= (mod (length hexstring) 2) 0)
+         (mapconcat 'identity (reverse (string-to-pairs hexstring)) ""))
+        (t (mapconcat 'reverse-string (string-to-pairs (reverse-string hexstring)) ""))))
+
+(defun endian-swap-hex-at-point ()
+  (interactive)
+  (save-excursion
+    (skip-chars-backward "x0123456789abcdefABCDEF")
+    (or (looking-at "0x[0-9a-fA-F]\\{1,16\\}\\b")
+        (error "No hex at point"))
+    (replace-match (concat "0x" (endian-swap (substring (match-string 0) 2))))))
+
+(defun endian-swap-dec-at-point ()
+  (interactive)
+  (save-excursion
+    (skip-chars-backward "0-9")
+    (or (looking-at "[0-9]+")
+        (error "No number at point"))
+    (replace-match (hex-to-dec (endian-swap (dec-to-hex (match-string 0) ""))) t)))
+
+(defun toggle-endianness-at-point ()
+  (interactive)
+  (condition-case nil
+      (endian-swap-hex-at-point)
+    (error (endian-swap-dec-at-point))))
+
+(global-set-key "\C-ce" 'toggle-endianness-at-point)
