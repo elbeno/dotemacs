75cfcb025a00ebea009aab45d68ef62fa540652a
Ben Deane <ben@elbeno.com>
2019-04-16 19:44:21 -0600

    Python import management

--- .emacs.d/python-modes.el
+++ .emacs.d/python-modes.el
@@ -22,3 +22,25 @@
   :config
   (setq py-autopep8-options (list "--global-config=~/.config/flake8"))
   :hook (elpy-mode . py-autopep8-enable-on-save))
+
+
+
+;;------------------------------------------------------------------------------
+;; manage python imports
+(use-package pyimport
+  :ensure t
+  :hook
+  (elpy-mode . (lambda () (add-hook 'before-save-hook 'pyimport-remove-unused))))
+
+(use-package importmagic
+  :ensure t
+  :hook
+  (elpy-mode . (lambda ()
+                 (importmagic-mode)
+                 (add-hook 'before-save-hook 'importmagic-fix-imports))))
+
+(use-package pyimpsort
+  :ensure t
+  :hook
+  (elpy-mode . (lambda () (add-hook 'before-save-hook 'pyimpsort-buffer))))
+
