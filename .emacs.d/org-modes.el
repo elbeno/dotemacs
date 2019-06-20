85d2656c14ea6942df6f7cf86dfecaf8bb602137
Ben Deane <ben@elbeno.com>
2019-06-19 19:24:12 -0600

    Remove fci-mode provisions from org-mode code

--- .emacs.d/org-modes.el
+++ .emacs.d/org-modes.el
@@ -60,17 +60,10 @@
   :ensure t
   :defer)
 
-;; suspend fci mode and flyspell-mode when exporting html
-(defvar modi/htmlize-initial-fci-state nil
-  "Variable to store the state of `fci-mode' when `htmlize-buffer' is called.")
 (defvar modi/htmlize-initial-flyspell-state nil
   "Variable to store the state of `flyspell-mode' when `htmlize-buffer' is called.")
 
 (defun modi/htmlize-before-hook-fn ()
-  (when (fboundp 'fci-mode)
-    (setq modi/htmlize-initial-fci-state fci-mode)
-    (when fci-mode
-      (fci-mode -1)))
   (when (fboundp 'flyspell-mode)
     (setq modi/htmlize-initial-flyspell-state flyspell-mode)
     (when flyspell-mode
@@ -78,9 +71,6 @@
 (add-hook 'htmlize-before-hook #'modi/htmlize-before-hook-fn)
 
 (defun modi/htmlize-after-hook-fn ()
-  (when (fboundp 'fci-mode)
-    (when modi/htmlize-initial-fci-state
-      (fci-mode 1)))
   (when (fboundp 'flyspell-mode)
     (when modi/htmlize-initial-flyspell-state
       (flyspell-mode 1))))
