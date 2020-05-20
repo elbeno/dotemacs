a45298e6a0c5961e5d46fad12000cebf24f12bb6
Ben Deane <ben@elbeno.com>
2020-05-20 17:08:39 -0600

    Python mode tweaks: use black, remove packages that are flaky

--- .emacs.d/python-modes.el
+++ .emacs.d/python-modes.el
@@ -16,7 +16,7 @@
 
 (add-hook 'python-mode-hook
           (lambda () (bind-keys :map python-mode-map
-                           ("C-c f" . toggle-string-to-fstring))))
+                           ("C-c s" . toggle-string-to-fstring))))
 
 ;;------------------------------------------------------------------------------
 ;; elpy
@@ -24,41 +24,27 @@
   :ensure t
   :init
   (setq python-indent-offset 4
-        elpy-rpc-python-command "python3")
+        elpy-rpc-python-command "python3"
+        elpy-rpc-timeout 10)
   :bind (:map elpy-mode-map
-              ("M-k" . elpy-check))
+              ("M-k" . elpy-check)
+              ("C-c f" . elpy-black-fix-code))
   :hook (python-mode . elpy-mode))
 
 ;;------------------------------------------------------------------------------
-;; pyautopep8
-(use-package py-autopep8
-  :ensure t
-  :config
-  (setq py-autopep8-options (list "--global-config=~/.config/flake8")))
-
-;;------------------------------------------------------------------------------
 ;; manage python imports
-(use-package pyimport
-  :ensure t)
-
-(use-package importmagic
-  :ensure t)
-
 (use-package pyimpsort
   :ensure t)
 
 ;;------------------------------------------------------------------------------
-;; on save: fix imports, sort them, remove unused, then pep8
+;; on save: remove unused imports, sort them, then format with black
 (defun my-python-before-save-hook ()
   (save-excursion
-    (importmagic-fix-imports)
-    (pyimpsort-remove-unused)
-    (pyimpsort-buffer)
-    (py-autopep8-buffer)))
+    (elpy-black-fix-code)
+    (pyimpsort-buffer)))
 
 (add-hook 'elpy-mode-hook
-          (lambda () (importmagic-mode)
-            (add-hook 'before-save-hook 'my-python-before-save-hook t 'local)))
+          (lambda () (add-hook 'before-save-hook 'my-python-before-save-hook t 'local)))
 
 ;;------------------------------------------------------------------------------
 ;; jupyter
