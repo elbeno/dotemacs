5465a32b3e0befe907fbb3ce5d2dae47393dbc26
Ben Deane <ben@elbeno.com>
2021-09-07 21:14:27 -0600

    Generated custom file updates

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -6,7 +6,7 @@
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
  '(bm-cycle-all-buffers t)
- '(bm-marker (quote bm-marker-right) t)
+ '(bm-marker 'bm-marker-right t)
  '(case-fold-search t)
  '(comint-buffer-maximum-size 20000)
  '(comint-completion-addsuffix t)
@@ -18,52 +18,41 @@
  '(comint-scroll-show-maximum-output t)
  '(comint-scroll-to-bottom-on-input t)
  '(company-c-headers-path-system
-   (quote
-    ("/usr/include/" "/usr/local/include/" "/usr/include/c++/v1/")))
+   '("/usr/include/" "/usr/local/include/" "/usr/include/c++/v1/"))
  '(custom-safe-themes
-   (quote
-    ("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c280a47cb66453f297553efcd0152666bdaab65ef4e8dfc498973f81ec1b60bb" "01c3b475bc176a1eb66ad7f2513523f59fb131c58132b975b254624198c8fd41" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" default)))
+   '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "c280a47cb66453f297553efcd0152666bdaab65ef4e8dfc498973f81ec1b60bb" "01c3b475bc176a1eb66ad7f2513523f59fb131c58132b975b254624198c8fd41" "756597b162f1be60a12dbd52bab71d40d6a2845a3e3c2584c6573ee9c332a66e" default))
  '(ecb-layout-name "left3")
  '(ecb-options-version "2.40")
  '(ecb-tip-of-the-day nil)
  '(elpy-modules
-   (quote
-    (elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-sane-defaults)))
+   '(elpy-module-company elpy-module-eldoc elpy-module-pyvenv elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-sane-defaults))
  '(highlight-symbol-idle-delay 0)
  '(highlight-symbol-on-navigation-p t)
- '(org-export-with-sub-superscripts (quote {}))
+ '(org-export-with-sub-superscripts '{})
  '(package-selected-packages
-   (quote
-    (intero dired dired-git-info diredfl ein importmagic pyimpsort pyimport org-re-reveal py-autopep8 symbol-overlay yasnippet-snippets browse-kill-ring zzz-to-char filladapt string-inflection lsp-mode amx wttrin flycheck-clangcheck alchemist markdown-mode+ cmake-font-lock all-the-icons smart-mode-line moe-theme nyan-mode ripgrep emojify visual-regexp-steroids flycheck-post-tip feature-mode flyheck smart-tab lsp-clangd lsp-ui company-lsp modern-cpp-font-lock diminish ido-completing-read+ markdown-mode syntactic-close haskell-mode flycheck-pos-tip clang-format hlint-refactor hlint-refactor-mode htmlize company fm fill-column-indicator general-close ox-reveal pdf-tools company-shell company-quickhelp pos-tip yaml-mode neotree num3-mode org-bullets region-state diff-hl cmake-ide parent-mode yasnippet magit git-commit which-key expand-region elpy comment-dwim-2 ace-window avy company-ghci smartscan hindent elisp--witness--lisp jinja2-mode flycheck-haskell flycheck-irony flycheck vlf org-plus-contrib git-messenger git-timemachine git-gutter-fringe+ git-commit-mode gitignore-mode gitconfig-mode company-ghc protobuf-mode nxml-mode json-mode js2-mode lua-mode projectile cpputils-cmake cmake-mode company-c-headers company-irony irony etags-table etags-select multiple-cursors ztree google-this undo-tree smex ido-vertical-mode ido-ubiquitous flx-ido bm column-marker highlight-escape-sequences highlight-quoted highlight-numbers rainbow-mode rainbow-delimiters dash ws-butler cygwin-mount esup use-package)))
+   '(py-isort company-box intero dired dired-git-info diredfl ein importmagic pyimpsort pyimport org-re-reveal py-autopep8 symbol-overlay yasnippet-snippets browse-kill-ring zzz-to-char filladapt string-inflection lsp-mode amx wttrin flycheck-clangcheck alchemist markdown-mode+ cmake-font-lock all-the-icons smart-mode-line moe-theme nyan-mode ripgrep emojify visual-regexp-steroids flycheck-post-tip feature-mode flyheck smart-tab lsp-clangd lsp-ui company-lsp modern-cpp-font-lock diminish ido-completing-read+ markdown-mode syntactic-close haskell-mode flycheck-pos-tip clang-format hlint-refactor hlint-refactor-mode htmlize company fm fill-column-indicator general-close ox-reveal pdf-tools company-shell company-quickhelp pos-tip yaml-mode neotree num3-mode org-bullets region-state diff-hl cmake-ide parent-mode yasnippet magit git-commit which-key expand-region elpy comment-dwim-2 ace-window avy company-ghci smartscan hindent elisp--witness--lisp jinja2-mode flycheck-haskell flycheck-irony flycheck vlf org-plus-contrib git-messenger git-timemachine git-gutter-fringe+ git-commit-mode gitignore-mode gitconfig-mode company-ghc protobuf-mode nxml-mode json-mode js2-mode lua-mode projectile cpputils-cmake cmake-mode company-c-headers company-irony irony etags-table etags-select multiple-cursors ztree google-this undo-tree smex ido-vertical-mode ido-ubiquitous flx-ido bm column-marker highlight-escape-sequences highlight-quoted highlight-numbers rainbow-mode rainbow-delimiters dash ws-butler cygwin-mount esup use-package))
  '(protect-buffer-bury-p nil)
  '(rotate-text-patterns
-   (quote
-    (("-?0x?[0-9a-fA-F]+" rotate-text-increment-hex-number)
+   '(("-?0x?[0-9a-fA-F]+" rotate-text-increment-hex-number)
      ("\\_<[^-]\\(\\sw\\|\\s_\\)*[0-9]+" rotate-text-increment-number-in-symbol)
-     ("-?[0-9]+" rotate-text-increment-number))))
+     ("-?[0-9]+" rotate-text-increment-number)))
  '(rotate-text-symbols
-   (quote
-    (("static_cast" "dynamic_cast" "const_cast" "reinterpret_cast")
+   '(("static_cast" "dynamic_cast" "const_cast" "reinterpret_cast")
      ("class" "struct" "auto")
      ("and" "or" "not" "xor")
-     ("true" "false"))))
+     ("true" "false")))
  '(rotate-text-words
-   (quote
-    (("north" "south" "east" "west")
+   '(("north" "south" "east" "west")
      ("public" "protected" "private")
      ("width" "height")
-     ("left" "right" "top" "bottom"))))
+     ("left" "right" "top" "bottom")))
  '(safe-local-variable-values
-   (quote
-    ((eval when
-           (require
-            (quote rainbow-mode)
-            nil t)
-           (rainbow-mode 1)))))
+   '((eval when
+           (require 'rainbow-mode nil t)
+           (rainbow-mode 1))))
  '(vc-annotate-background "#2f2f2f")
  '(vc-annotate-color-map
-   (quote
-    ((20 . "#dc322f")
+   '((20 . "#dc322f")
      (40 . "#D01A4E")
      (60 . "#cb4b16")
      (80 . "#b58900")
@@ -80,7 +69,7 @@
      (300 . "#268bd2")
      (320 . "#268bd2")
      (340 . "#94BFF3")
-     (360 . "#d33682"))))
+     (360 . "#d33682")))
  '(vc-annotate-very-old-color "#d33682")
  '(vc-follow-symlinks nil))
 (custom-set-faces
