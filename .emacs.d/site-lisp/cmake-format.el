ae9de239886434378ee454123deebb390ae94010
Ben Deane <ben@elbeno.com>
2022-10-09 18:49:25 -0600

    Allow cmake-format config file

--- .emacs.d/site-lisp/cmake-format.el
+++ .emacs.d/site-lisp/cmake-format.el
@@ -14,11 +14,27 @@ A string containing the name or the full path of the executable."
   :type '(file :must-match t)
   :risky t)
 
+(defcustom cmake-format-config-file
+  ".cmake-format.yaml"
+  "Name of the cmake-format config file.
+
+A string containing the name (without path) of the cmake-format config file."
+  :group 'cmake-format
+  :type '(file :must-match t)
+  :risky t)
+
+(defun cmake-config-argument (filename)
+  (if (> (length cmake-format-config-file) 0)
+      `("-c" ,(concat (locate-dominating-file filename cmake-format-config-file)
+                      cmake-format-config-file))
+    nil))
+
 ;;;###autoload
 (defun cmake-format-buffer ()
   "Use cmake-format to format the current buffer."
   (interactive)
-  (let ((filename (make-temp-file "tmp-cmake-format")))
+  (let ((filename (make-temp-file "tmp-cmake-format"))
+        (config-arg (cmake-config-argument (buffer-file-name))))
     ;; write current buffer to temp file
     (let ((coding-system-for-write 'binary)
           (contents (buffer-string)))
@@ -28,7 +44,9 @@ A string containing the name or the full path of the executable."
     ;; run cmake-format on temp file
     (let ((old-point (point))
           (replacement (with-temp-buffer
-                         (when (eq (call-process cmake-format-executable nil t nil filename) 0)
+                         (when (eq (apply #'call-process
+                                          (append `(,cmake-format-executable nil t nil ,filename)
+                                                  config-arg)) 0)
                            (buffer-string)))))
       ;; replace contents of current buffer
       (when (not (= (length replacement) 0))
