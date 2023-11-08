6e8c138ce489ae80288e94077fa80deb65fb5088
Ben Deane <ben@elbeno.com>
2023-11-07 19:49:36 -0700

    Add fzf

--- .emacs.d/common.el
+++ .emacs.d/common.el
@@ -513,3 +513,23 @@
                   '((lambda (x n) (number-to-string n)) . 0)
                   nil (region-beginning) (region-end)))
 (bind-key "C-S-#" 'replace-numbers-in-region-with-increments)
+
+;;------------------------------------------------------------------------------
+;; fuzzy find
+(use-package fzf
+  :ensure t
+  :bind
+  (("C-c z f" . fzf-find-file)
+   ("C-c z g" . fzf-grep)
+   ("C-c z z" . fzf))
+  :config
+  (setq fzf/args "-x --color bw --print-query --margin=1,0 --no-hscroll"
+        fzf/executable "fzf"
+        fzf/git-grep-args "-i --line-number %s"
+        ;; command used for `fzf-grep-*` functions
+        ;; example usage for ripgrep:
+        fzf/grep-command "rg --no-heading -nH"
+        ;; fzf/grep-command "grep -nrH"
+        ;; If nil, the fzf buffer will appear at the top of the window
+        fzf/position-bottom t
+        fzf/window-height 15))
