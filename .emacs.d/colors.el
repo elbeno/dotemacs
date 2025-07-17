879c12f7006878d5e0a208c67dcf37917ae5aa28
Ben Deane <ben@elbeno.com>
2025-07-16 21:10:29 -0600

    Add doom themes

--- .emacs.d/colors.el
+++ .emacs.d/colors.el
@@ -1,19 +1,24 @@
 ;; -*- lexical-binding: t; -*-
 ;;------------------------------------------------------------------------------
-;; Disable themes completely before applying a new one
-(defadvice load-theme (before theme-dont-propagate activate)
-  "Disable all theme effects before enabling new ones."
-  (mapc #'disable-theme custom-enabled-themes))
-
-;;------------------------------------------------------------------------------
-;; Nice theme
+;; Nice themes
 (defun load-theme-cyberpunk ()
   (interactive)
+  (mapc #'disable-theme custom-enabled-themes)
   (load-theme 'cyberpunk t))
 
 (use-package cyberpunk-theme
   :ensure t
   :bind (("C-c t c" . load-theme-cyberpunk)))
+
+(defun load-theme-tomorrow-night-deepblue ()
+  (interactive)
+  (mapc #'disable-theme custom-enabled-themes)
+  (load-theme 'tomorrow-night-deepblue t))
+
+(use-package tomorrow-night-deepblue-theme
+  :ensure t
+  :bind (("C-c t b" . load-theme-tomorrow-night-deepblue)))
+
 (load-theme-cyberpunk)
 
 ;;------------------------------------------------------------------------------
@@ -100,3 +105,137 @@
   :config
   (setq ef-themes-to-toggle '(ef-summer ef-winter))
   :bind (("C-c t e" . ef-themes-select)))
+
+;;------------------------------------------------------------------------------
+;; doom themes
+(use-package doom-themes
+  :ensure t
+  :custom
+  ;; Global settings (defaults)
+  (doom-themes-enable-bold t)   ; if nil, bold is universally disabled
+  (doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  ;; for treemacs users
+  (doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
+  :config
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (nerd-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config)
+  :bind (("C-c t D" . my/doom-themes-select)))
+
+;;------------------------------------------------------------------------------
+;; doom themes selection, piggybacking on ef-themes selection
+(defconst my/doom-themes
+  '(doom-1337
+    doom-acario-dark
+    doom-acario-light
+    doom-ayu-dark
+    doom-ayu-light
+    doom-ayu-mirage
+    doom-badger
+    doom-challenger-deep
+    doom-city-lights
+    doom-dark+
+    doom-dracula
+    doom-earl-grey
+    doom-ephemeral
+    doom-fairy-floss
+    doom-feather-dark
+    doom-feather-light
+    doom-flatwhite
+    doom-gruvbox
+    doom-gruvbox-light
+    doom-henna
+    doom-homage-black
+    doom-homage-white
+    doom-horizon
+    doom-Iosvkem
+    doom-ir-black
+    doom-lantern
+    doom-laserwave
+    doom-manegarm
+    doom-material
+    doom-material-dark
+    doom-meltbus
+    doom-miramare
+    doom-molokai
+    doom-monokai-classic
+    doom-monokai-machine
+    doom-monokai-octagon
+    doom-monokai-pro
+    doom-monokai-ristretto
+    doom-monokai-spectrum
+    doom-moonlight
+    doom-nord
+    doom-nord-aurora
+    doom-nord-light
+    doom-nova
+    doom-oceanic-next
+    doom-oksolar-dark
+    doom-oksolar-light
+    doom-old-hope
+    doom-one
+    doom-one-light
+    doom-opera
+    doom-opera-light
+    doom-outrun-electric
+    doom-palenight
+    doom-peacock
+    doom-pine
+    doom-plain
+    doom-plain-dark
+    doom-rouge
+    doom-shades-of-purple
+    doom-snazzy
+    doom-solarized-dark
+    doom-solarized-dark-high-contrast
+    doom-solarized-light
+    doom-sourcerer
+    doom-spacegrey
+    doom-tokyo-night
+    doom-tomorrow-day
+    doom-tomorrow-night
+    doom-vibrant
+    doom-wilmersdorf
+    doom-xcode
+    doom-zenburn)
+  "List of doom themes.")
+
+(defun my/doom-themes--enable-themes ()
+  "Enable all doom themes."
+  (mapc
+   (lambda (theme)
+     (unless (memq theme custom-known-themes)
+       (load-theme theme :no-confirm :no-enable)))
+   my/doom-themes))
+
+(defun my/doom-themes--load ()
+  "Return all the known doom themes."
+  (ef-themes--completion-table 'theme (my/doom-themes--enable-themes)))
+
+(defun my/doom-themes--select-prompt (&optional prompt)
+  "Minibuffer prompt for `doom-themes-select'.
+With optional PROMPT string, use it.  Else use a generic prompt."
+  (require 'ef-themes)
+  (let* ((themes (my/doom-themes--load))
+         (completion-extra-properties `(:annotation-function ,#'ef-themes--annotate-theme)))
+    (intern
+     (completing-read
+      (or prompt "Select Doom Theme: ")
+      themes
+      nil t nil
+      'my/doom-themes--select-theme-history))))
+
+(defvar my/doom-themes--select-theme-history nil
+  "Minibuffer history of `my/doom-themes--select-prompt'.")
+
+(defun my/doom-themes-select (theme)
+  "Load a doom THEME using minibuffer completion.
+When called from Lisp, THEME is the symbol of a theme."
+  (interactive (list (my/doom-themes--select-prompt nil)))
+  (mapc #'disable-theme custom-enabled-themes)
+  (load-theme theme t))
