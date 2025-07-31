33a142063dcf37b05e3477b60e3458219006c271
Ben Deane <ben@elbeno.com>
2025-07-31 13:28:36 -0600

    Add a help transient

--- .emacs.d/common.el
+++ .emacs.d/common.el
@@ -760,3 +760,93 @@
       (add-hook 'pre-command-hook 'keycast--update t)
       (message "Keycast ON")))
   :bind ("C-c t k" . my/toggle-keycast))
+
+;;------------------------------------------------------------------------------
+;;; A Help Transient on C-S-h
+(use-package helpful
+  :ensure t
+  :bind ("C-h f" . #'helpful-callable)
+  ("C-h v" . #'helpful-variable)
+  ("C-h k" . #'helpful-key)
+  ("C-h x" . #'helpful-command))
+
+(transient-define-prefix hrm-help-transient ()
+  "Help commands that I use. A subset of C-h with others thrown in."
+  ["Help Commands"
+   ["Mode & Bindings"
+    ("m" "Mode" describe-mode)
+    ("M" "Minor Modes" consult-minor-mode-menu)
+    ("b" "Major Bindings" which-key-show-full-major-mode)
+    ("B" "Minor Bindings" which-key-show-full-minor-mode-keymap)
+    ("d" "Descbinds" describe-bindings) ; or embark-bindings
+    ("t" "Top Bindings  " which-key-show-top-level)
+    ]
+   ["Describe"
+    ("C" "Command" helpful-command)
+    ("f" "Function" helpful-callable)
+    ("v" "Variable " helpful-variable)
+    ("k" "Key" helpful-key)
+    ("s" "Symbol" helpful-symbol)
+    ("l" "Library" apropos-library)
+    ]
+   ["Info on"
+    ("C-c" "Command" Info-goto-emacs-command-node)
+    ("C-f" "Function" info-lookup-symbol)
+    ("C-v" "Variable" info-lookup-symbol) ; fails if transient-detect-key-conflicts
+    ("C-k" "Key" Info-goto-emacs-key-command-node)
+    ("C-s" "Symbol" info-lookup-symbol)
+    ]
+   ["Goto Source"
+    ""
+    ("F" "Function" find-function-other-frame)
+    ("V" "Variable" find-variable-other-frame)
+    ("K" "Key" find-function-on-key-other-frame)
+    ""
+    ("L" "Library" find-library-other-frame)
+    ]
+   ["Apropos"
+    ("ac" "Command" apropos-command)
+    ("af" "Function" apropos-function)
+    ("av" "Variable" apropos-variable)
+    ("aV" "Value" apropos-value)
+    ("aL" "Local Value" apropos-local-value)
+    ("ad" "Documentation" apropos-documentation)
+    ]
+   ]
+  [
+   ["Internals"
+    ("I" "Input Method" describe-input-method)
+    ("G" "Language Env" describe-language-environment)
+    ("S" "Syntax" describe-syntax)
+    ("T" "Categories" describe-categories)
+    ("O" "Coding System" describe-coding-system)
+    ("o" "Coding Briefly" describe-current-coding-system-briefly)
+    ("T" "Display Table" describe-current-display-table)
+    ("e" "Echo Messages" view-echo-area-messages)
+    ("H" "Lossage" view-lossage)
+    ]
+   ["Describe"
+    ("." "At Point" helpful-at-point)
+    ("c" "Key Short" describe-key-briefly)
+    ("p" "Key Map" describe-keymap)
+    ("A" "Face" describe-face)
+    ("i" "Icon" describe-icon)
+    ("w" "Where Is" where-is)
+    ("=" "Position" what-cursor-position)
+    ("g" "Shortdoc" shortdoc-display-group)
+    ]
+   ["Info Manuals"
+    ("C-i" "Info" info)
+    ("C-4" "Other Window" info-other-window)
+    ("C-e" "Emacs" info-emacs-manual)
+    ("C-l" "Elisp" info-elisp-manual)
+    ("C-r" "Pick Manual" info-display-manual)
+    ]
+   ["External"
+    ("C" "cppreference" duckduckgo-this-cpp-reference)
+    ("N" "Man" consult-man)
+    ("W" "Dictionary" powerthesaurus-lookup-dwim)
+    ]
+   ]
+  )
+(global-set-key (kbd "C-S-h") 'hrm-help-transient)
