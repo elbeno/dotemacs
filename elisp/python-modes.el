42e7d3554bbc86ca8f437776cf0535ea4537c6cf
Ben Deane <ben@elbeno.com>
2026-05-21 22:12:03 -0600

    Eglot python mode with ruff and ty

--- elisp/python-modes.el
+++ elisp/python-modes.el
@@ -15,21 +15,41 @@
 ;; eglot
 (use-package eglot
   :ensure t
+  :commands (eglot eglot-ensure)
+  :custom
+  (eglot-autoshutdown t)
+  (eglot-sync-connect 0)
+  (eglot-send-changes-idle-time 0.5)
+  (eglot-extend-to-xref t)
+  (eglot-events-buffer-size 0)
+  (eglot-inlay-hints-mode -1)
   :config
-  (defun my/eglot-organize-imports () (interactive)
-	 (eglot-code-actions (point-min) (point-max) "source.organizeImports" t))
-  (defun my/python-eglot-config ()
-    (eglot-ensure)
-    (add-hook 'before-save-hook 'eglot-format-buffer nil t)
-    (add-hook 'before-save-hook 'my/eglot-organize-imports nil t))
-  (fset #'jsonrpc--log-event #'ignore)
+  (add-to-list 'eglot-server-programs '(python-ts-mode . ("ty" "server")))
   :bind (:map eglot-mode-map
-              ("C-c F" . eglot-format)
-              ("C-c i" . my/eglot-organize-imports)
-              ("C-c r" . eglot-rename)
-              ("C-c s" . toggle-string-to-fstring)
-              ("C-<return>" . eglot-code-actions)
+              ("C-c l a" . eglot-code-actions)
+              ("C-c l d" . eldoc)
+              ("C-c l f" . eglot-format)
+              ("C-c l h" . eglot-inlay-hints-mode)
+              ("C-c l i" . eglot-code-action-organize-imports)
+              ("C-c l l" . org-store-link)
+              ("C-c l q" . eglot-shutdown-all)
+              ("C-c l r" . eglot-rename)
               ("M-<up>" . flymake-goto-prev-error)
-              ("M-<down>" . flymake-goto-next-error))
-  :hook ((python-mode . my/python-eglot-config)
-         (python-ts-mode . my/python-eglot-config)))
+              ("M-<down>" . flymake-goto-next-error)
+              ("C-<return>" . eglot-code-action-quickfix))
+  :hook ((python-ts-mode . eglot-ensure)))
+
+;; inlay hints off by default
+(add-hook 'eglot-managed-mode-hook (lambda () (eglot-inlay-hints-mode -1)))
+
+;;------------------------------------------------------------------------------
+;; Use ruff with apheleia
+(with-eval-after-load 'apheleia
+  (when (executable-find "ruff")
+    (setf (alist-get 'python-ts-mode apheleia-mode-alist) '(ruff-isort ruff))))
+
+;; Use ruff with flymake
+(when (executable-find "ruff")
+  (use-package flymake-ruff
+    :ensure t
+    :hook (eglot-managed-mode . flymake-ruff-load)))
